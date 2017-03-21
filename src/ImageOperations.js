45c94f100a9f04056cc96b019d25d9de6197afa8
Christopher Watts <chris@byhook.com>
2017-03-21 14:12:24 -0700

    refactor size calc from buffer

--- src/ImageOperations.js
+++ src/ImageOperations.js
@@ -5,32 +5,26 @@
 const _ = require('lodash');
 const Jimp = require('jimp');
 const imageSize = require('image-size');
-
-const kb=1024;
-
+const kb = 1024;
 let sizeAndScale = function (imagePath, outputPath, scale = 1, sizeInKB = 100) {
   return new Promise(function (resolve, reject) {
-    Jimp.read(imagePath).then(function (image) {
-  
-      let size = 0;
-  
-      let imageBuffer = image.getBuffer (Jimp.AUTO,function (err,value){
-        size = imageSize (value);
-        size
-        
-      });
+    Jimp.read(imagePath).then(bufferReport).then (function (report){
       
-     
-      
+      console.log (report)
       
     })
   })
 };
 
 
-
+let bufferReport = function (image) {
+  return new Promise(function (resolve, reject) {
+    image.getBuffer(Jimp.AUTO, function (err, value) {
+      let size = value.byteLength;
+      resolve({size: size, kb: Math.ceil(size / kb)})
+    });
+  })
+};
 module.exports = {
-  
-  sizeAndScale : sizeAndScale
-  
+  sizeAndScale: sizeAndScale
 };
