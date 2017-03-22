c03f4c0a55c358557c1160ae91046ba9885263d3
Christopher Watts <chris@byhook.com>
2017-03-21 17:47:36 -0700

    clean up

--- index.js
+++ index.js
@@ -3,58 +3,24 @@
  */
 "use strict";
 const _ = require('lodash');
-const jimp = require ('jimp');
-const glob = require ('glob');
-const path = require ('path');
-
-
-
-const sizeAndScale = require ('./src/ImageOperations').sizeAndScale;
-
-const source = path.resolve(process.cwd() ,'_source');
-const dist = path.resolve(process.cwd() ,'_out');
-
-
-let run = function (){
-  
-  console.log ('hello world');
-  
-  let files = findInDir(source,'**/*.png');
-  
-  
+const jimp = require('jimp');
+const glob = require('glob');
+const path = require('path');
+const sizeAndScale = require('./src/ImageOperations').sizeAndScale;
+const source = path.resolve(process.cwd(), '_source');
+const dist = path.resolve(process.cwd(), '_out');
+let run = function () {
+  console.log('hello world');
+  let files = findInDir(source, '**/*.png');
   let ip = [];
-  
-  _.forEach(files,function (value,index) {
-  
-    ip.push (sizeAndScale (
-  
-      path.resolve (source,value),
-      path.resolve (dist,value)
-      
+  _.forEach(files, function (value, index) {
+    ip.push(sizeAndScale(
+      path.resolve(source, value),
+      path.resolve(dist, value)
     ))
-    
-    
-    
   })
-  
-  
 };
-
-
-
-let findInDir = function (dir,pattern) {
-  
-  
-  return glob.sync (pattern, {cwd:dir} )
-  
-  
+let findInDir = function (dir, pattern) {
+  return glob.sync(pattern, {cwd: dir})
 };
-
-
-
-
-
-
-
-
 run();
